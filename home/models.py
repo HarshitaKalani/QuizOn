5424bd99350c8385fa4594b5d2eef9a1dfb1bc5b
HarshitaKalani <95532771+HarshitaKalani@users.noreply.github.com>
2022-03-10 22:51:02 +0530

    image

--- home/models.py
+++ home/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 from django.db.models.fields import IntegerField
 from django.forms import ModelForm
@@ -121,6 +122,7 @@ class QuestionFinal(models.Model):
     questionTimer=models.IntegerField(default=10)
     marks=models.IntegerField(default=10)
     que=RichTextField(default=" ")
+    queImage = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100, default=None)
     number = models.IntegerField()
     op1=RichTextField(default=" ")
     op2=RichTextField(default=" ")
